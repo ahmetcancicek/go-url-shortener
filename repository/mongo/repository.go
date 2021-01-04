5f9baaa8ac2d24701d2cc58ebaec94b068aacbeb
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-01-04 22:14:24 +0300

    Implement function for redirect repository

--- repository/mongo/repository.go
+++ repository/mongo/repository.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"github.com/ahmetcancicek/go-url-shortener/shortener"
 	"github.com/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
 	"log"
 	"time"
 )
@@ -39,4 +41,62 @@ func NewRepository(url, db string) (shortener.RedirectRepository, error) {
 	}
 	repo.client = client
 	return repo, nil
+}
+
+func (r Repository) FindByCode(code string) (*shortener.Redirect, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	redirect := &shortener.Redirect{}
+	collection := r.client.Database(r.database).Collection("redirects")
+	filter := bson.M{"code": code}
+	err := collection.FindOne(ctx, filter).Decode(&redirect)
+	if err != nil {
+		return nil, errors.Wrap(err, "repository.Redirect.FindByCode")
+	}
+
+	return redirect, nil
+}
+
+func (r Repository) FindByID(id string) (*shortener.Redirect, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	redirect := &shortener.Redirect{}
+	collection := r.client.Database(r.database).Collection("redirects")
+	filter := bson.M{"id": id}
+	err := collection.FindOne(ctx, filter).Decode(&redirect)
+	if err != nil {
+		return nil, errors.Wrap(err, "repository.Redirect.FindByID")
+	}
+
+	return redirect, nil
+}
+
+func (r Repository) Save(redirect *shortener.Redirect) (*shortener.Redirect, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Generate ID
+	redirect.ID, _ = uuid.New()
+
+	// Save
+	collection := r.client.Database(r.database).Collection("redirects")
+	_, err := collection.InsertOne(ctx, bson.M{
+		"ID":         redirect.ID,
+		"code":       redirect.Code,
+		"url":        redirect.URL,
+		"click":      redirect.Click,
+		"created_at": redirect.CreatedAt,
+	})
+	if err != nil {
+		errors.Wrap(err, "repository.Redirect.Save")
+	}
+
+	return redirect, err
+}
+
+func (r Repository) Update(redirect *shortener.Redirect) (*shortener.Redirect, error) {
+	// TODO: Implement this function
+	panic("implement me")
 }
