51eb3ebafecebd8c6e51b0c09d98fbc0a804bb31
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-01-04 21:41:06 +0300

    Add abstract function for service layer

--- shortener/service.go
+++ shortener/service.go
@@ -1,4 +1,8 @@
 package shortener
 
 type RedirectService interface {
+	FindByCode(code string) (*Redirect, error)
+	FindByID(id string) (*Redirect, error)
+	Save(redirect *Redirect) (*Redirect, error)
+	Update(redirect *Redirect) (*Redirect, error)
 }
