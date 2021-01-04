70ba4edb58d70b82e60ba88e27165e36a9b186d2
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-01-04 21:48:51 +0300

    Add abstract function

--- shortener/repository.go
+++ shortener/repository.go
@@ -1,4 +1,8 @@
 package shortener
 
 type RedirectRepository interface {
+	FindByCode(code string) (*Redirect, error)
+	FindByID(id string) (*Redirect, error)
+	Save(redirect *Redirect) (*Redirect, error)
+	Update(redirect *Redirect) (*Redirect, error)
 }
