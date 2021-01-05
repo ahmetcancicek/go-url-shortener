12ee543c8fb8db4ec0dc7019ff6f80fdcd1544bb
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-01-05 19:54:05 +0300

    Fix functions

--- shortener/repository.go
+++ shortener/repository.go
@@ -2,7 +2,5 @@ package shortener
 
 type RedirectRepository interface {
 	FindByCode(code string) (*Redirect, error)
-	FindByID(id uint) (*Redirect, error)
 	Save(redirect *Redirect) (*Redirect, error)
-	Update(redirect *Redirect) (*Redirect, error)
 }
--- shortener/service.go
+++ shortener/service.go
@@ -2,7 +2,5 @@ package shortener
 
 type RedirectService interface {
 	FindByCode(code string) (*Redirect, error)
-	FindByID(id uint) (*Redirect, error)
 	Save(redirect *Redirect) (*Redirect, error)
-	Update(redirect *Redirect) (*Redirect, error)
 }
