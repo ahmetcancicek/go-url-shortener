1f4046e6fe368b805f21c6b23e931d6df661f98e
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-01-05 14:21:11 +0300

    Add response struct and function for JSON message

---
+++ shortener/response.go
@@ -0,0 +1,38 @@
+package shortener
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Code    int      `json:"code"`
+	Status  string   `json:"status"`
+	Message string   `json:"message"`
+	Errors  []string `json:"errors"`
+}
+
+func RespondWithError(w http.ResponseWriter, code int, message string) {
+	RespondWithJSON(w, code,
+		Response{
+			Code:    code,
+			Status:  "Error",
+			Message: message,
+		})
+}
+
+func RespondWithErrors(w http.ResponseWriter, code int, message string, errors []string) {
+	RespondWithJSON(w, code, Response{
+		Code:    code,
+		Status:  "Error",
+		Message: message,
+		Errors:  errors,
+	})
+}
+
+func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
