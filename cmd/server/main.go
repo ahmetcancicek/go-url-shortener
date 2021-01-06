d003964133e82f0afd9f2c94f379bc57c63e3c71
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-01-06 22:32:48 +0300

    Implement MongoDB for init

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"github.com/ahmetcancicek/go-url-shortener/repository/mongo"
+	"github.com/ahmetcancicek/go-url-shortener/shortener"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"net/http"
+)
 
-func main(){
-	fmt.Print("Hello World")
+const (
+	DBName = "url-shortener"
+	URI    = "mongodb://127.19.0.2:27017"
+)
+
+func main() {
+
+	credential := options.Credential{
+		Username: "admin",
+		Password: "password",
+	}
+
+	//1- DB mongodb://localhost:27017
+	repo, err := mongo.NewRepository(URI, DBName, credential, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+	service := shortener.NewRedirectService(repo)
+	handler := shortener.NewHandler(service)
+
+	// 2- Router
+	h := mux.NewRouter()
+	h.HandleFunc("/v1/redirect", handler.CreateRedirect()).Methods(http.MethodPost)
+
+	// 3- Server
+	srv := &http.Server{
+		Addr:    ":8500",
+		Handler: h,
+	}
+
+	// 4- Listen
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
