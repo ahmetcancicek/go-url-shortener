b8d9490d5fed93057a21a6d5bff8605dba497e28
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-01-07 20:38:39 +0300

    Refactor MongoDB repository

--- repository/mongo/repository.go
+++ repository/mongo/repository.go
@@ -14,17 +14,37 @@ import (
 
 type Repository struct {
 	client     *mongo.Client
-	database   string
+	dbURL      string
+	dbName     string
 	timeout    time.Duration
 	credential options.Credential
 }
 
-func NewClient(url string, credential options.Credential, timeout int) (*mongo.Client, error) {
+func NewRepository(dbURL, username, password, dbName string, timeout int) (shortener.RedirectRepository, error) {
+	repo := &Repository{
+		dbURL:   dbURL,
+		dbName:  dbName,
+		timeout: time.Duration(timeout),
+		credential: options.Credential{
+			Username: username,
+			Password: password,
+		},
+	}
+
+	client, err := newClient(repo)
+	if err != nil {
+		return nil, errors.Wrap(err, "repository.NewRepository")
+	}
+	repo.client = client
+	return repo, nil
+}
+
+func newClient(repo *Repository) (*mongo.Client, error) {
 	// Initialize a new mongo client with options
-	client, err := mongo.NewClient(options.Client().ApplyURI(url).SetAuth(credential))
+	client, err := mongo.NewClient(options.Client().ApplyURI(repo.dbURL).SetAuth(repo.credential))
 
 	// Connect the mongo client to the MongoDB server
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), repo.timeout)
 	err = client.Connect(ctx)
 
 	//To close the connection at the end
@@ -34,71 +54,30 @@ func NewClient(url string, credential options.Credential, timeout int) (*mongo.C
 	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Ping(context.Background(), readpref.Primary())
 	if err != nil {
-		return nil, errors.Wrap(err, "repository.NewClient")
+		return nil, errors.Wrap(err, "repository.newClient")
 	} else {
-		log.Println("Connected!")
+		log.Println("MongoDB Connected!")
 	}
 
 	return client, nil
 }
 
-func NewRepository(url, db string, credential options.Credential, timeout int) (shortener.RedirectRepository, error) {
-	repo := &Repository{
-		database: db,
-	}
-	client, err := NewClient(url, credential, timeout)
-	if err != nil {
-		return nil, errors.Wrap(err, "repository.NewRepository")
-	}
-	repo.client = client
-	return repo, nil
-}
-
 func (r Repository) FindByCode(code string) (*shortener.Redirect, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
 	redirect := &shortener.Redirect{}
-	collection := r.client.Database(r.database).Collection("redirects")
+	collection := r.client.Database(r.dbName).Collection("redirects")
 	filter := bson.M{"code": code}
-	err := collection.FindOne(ctx, filter).Decode(&redirect)
+	err := collection.FindOne(context.TODO(), filter).Decode(&redirect)
 	if err != nil {
 		return nil, errors.Wrap(err, "repository.Redirect.FindByCode")
 	}
-
-	return redirect, nil
-}
-
-func (r Repository) FindByID(id uint) (*shortener.Redirect, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	redirect := &shortener.Redirect{}
-	collection := r.client.Database(r.database).Collection("redirects")
-	filter := bson.M{"id": id}
-	err := collection.FindOne(ctx, filter).Decode(&redirect)
-	if err != nil {
-		return nil, errors.Wrap(err, "repository.Redirect.FindByID")
-	}
-
 	return redirect, nil
 }
 
 func (r Repository) Save(redirect *shortener.Redirect) (*shortener.Redirect, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), r.timeout)
-	defer cancel()
-	collection := r.client.Database(r.database).Collection("redirects")
-	_, err := collection.InsertOne(ctx,
-		bson.M{
-			"code":       redirect.Code,
-			"url":        redirect.URL,
-			"created_at": redirect.CreatedAt,
-			"click":      redirect.Click,
-		},
-	)
+	collection := r.client.Database(r.dbName).Collection("redirects")
+	_, err := collection.InsertOne(context.TODO(), redirect)
 	if err != nil {
 		errors.Wrap(err, "repository.Redirect.Save")
 	}
-
 	return redirect, err
 }
