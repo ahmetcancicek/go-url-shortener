42ba5e288440fcf76cc23e67ca7f4cb112e8e870
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-01-07 21:11:33 +0300

    Update main.go for new changed

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,25 +4,22 @@ import (
 	"github.com/ahmetcancicek/go-url-shortener/repository/mongo"
 	"github.com/ahmetcancicek/go-url-shortener/shortener"
 	"github.com/gorilla/mux"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"net/http"
 )
 
 const (
-	DBName = "url-shortener"
-	URI    = "mongodb://127.19.0.2:27017"
+	DBName   = "url-shortener"
+	URI      = "mongodb://localhost:27017"
+	USERNAME = "admin"
+	PASSWORD = "password"
+	TIMEOUT  = 10
 )
 
 func main() {
 
-	credential := options.Credential{
-		Username: "admin",
-		Password: "password",
-	}
-
 	//1- DB mongodb://localhost:27017
-	repo, err := mongo.NewRepository(URI, DBName, credential, 10)
+	repo, err := mongo.NewRepository(URI, USERNAME, PASSWORD, DBName, TIMEOUT)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -32,6 +29,9 @@ func main() {
 	// 2- Router
 	h := mux.NewRouter()
 	h.HandleFunc("/v1/redirect", handler.CreateRedirect()).Methods(http.MethodPost)
+	h.HandleFunc("/v1/redirect/{code}", handler.FindRedirectByCode()).Methods(http.MethodGet)
+
+	// db.createUser({user:'admin',pwd:'password',roles:[{role:'readWrite',db:'url-shortener'}]})
 
 	// 3- Server
 	srv := &http.Server{
