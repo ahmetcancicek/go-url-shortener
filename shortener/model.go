4412e77f724acd7229a9478cf5adce1b89b24671
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-01-10 20:05:07 +0300

    Update redirect model for validate

--- shortener/model.go
+++ shortener/model.go
@@ -6,7 +6,7 @@ import (
 
 type Redirect struct {
 	Code      string    `json:"code"`
-	URL       string    `json:"url"`
+	URL       string    `json:"url" validate:"required,url,min=4"`
 	Click     int       `json:"click"`
 	CreatedAt time.Time `json:"created_at"`
 }
