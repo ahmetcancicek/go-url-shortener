9e3f427f5faa07724fe86251682bfcf5b99c2a9c
Ahmet Can Çiçek <ahmetcan@ahmetcancicek.com>
2021-01-18 21:14:12 +0300

    Delete config package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/ahmetcancicek/go-url-shortener/config"
 	handler2 "github.com/ahmetcancicek/go-url-shortener/internal/app/shortener/handler"
 	"github.com/ahmetcancicek/go-url-shortener/internal/app/shortener/repository/mongo"
 	service2 "github.com/ahmetcancicek/go-url-shortener/internal/app/shortener/service"
@@ -13,36 +12,30 @@ import (
 
 func main() {
 
-	// Config SetUp
-	cfg, err := config.SetUp()
+	// DB Connect
+	repo, err := mongo.NewRepository("mongodb://localhost:27017", "url-shortener", "admin", "password", 10)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	repo, err := mongo.NewRepository(
-		cfg.Database.URI,
-		cfg.Database.Name,
-		cfg.Database.Username,
-		cfg.Database.Password,
-		cfg.Database.Timeout,
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Repository and service layer
 	service := service2.NewRedirectService(repo)
 	handler := handler2.NewHandler(service)
 
+	// Router
 	h := mux.NewRouter()
 	h.HandleFunc("/api/v1/redirect", handler.CreateRedirect()).Methods(http.MethodPost)
 	h.HandleFunc("/api/v1/redirect/{code}", handler.FindRedirectByCode()).Methods(http.MethodGet)
 
+	// Server
 	srv := &http.Server{
-		Addr:         ":" + cfg.Server.Port,
+		Addr:         ":8500",
 		ReadTimeout:  10 * time.Duration(time.Second),
 		WriteTimeout: 10 * time.Duration(time.Second),
 		Handler:      h,
 	}
 
+	// Start Server
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
